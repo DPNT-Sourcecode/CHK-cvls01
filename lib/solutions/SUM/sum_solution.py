29d2d949a24fd13a2166b02e42f9616c046b9df8
sbx_user1051 <sbx_user1051@169.254.149.125>
2022-05-18 09:53:28 +0000

    Wed May 18 09:53:28 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,10 +2,10 @@
 def compute(x, y):
     x = int(x)
     y = int(y)
-    if x >= 0 and x <= 100 and y >= 0 and y <= 100
-
-
-
-    a = [x, y]
-    return sum(a)
+    if 0 <= x <= 100 and 0 <= y <= 100:
+        a = [x, y]
+        return sum(a)
+    else:
+        return "Error - please check the numbers are integers between 0 and 100"
     #raise NotImplementedError()
+
