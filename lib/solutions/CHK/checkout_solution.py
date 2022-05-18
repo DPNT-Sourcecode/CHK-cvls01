1c9183e783ddfeac9c6e24cab41573fe5ad4ff81
sbx_user1051 <sbx_user1051@169.254.149.125>
2022-05-18 11:54:56 +0000

    Wed May 18 11:54:56 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,10 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    str(skus).
+    skus = str(skus).upper()  # Convert string to Upper Case
+
+
     #raise NotImplementedError()
+
 
 
