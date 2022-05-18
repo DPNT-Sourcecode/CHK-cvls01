7b6b1e89bb6278f42b49de6a87fae3aa5f4dd27f
sbx_user1051 <sbx_user1051@169.254.149.125>
2022-05-18 12:54:57 +0000

    Wed May 18 12:54:57 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -17,8 +17,38 @@ def checkout(skus):
         #Total Up cost of occurrences of Item A
 
         if a_count > 0:
-            
+            if a_count//3 > 0:  # some will be at discount cost
+                a_total_discount_grouping_cost = a_count//3
+                a_single_normal_cost = a_count % 3
+                a_total_cost = (130 * a_total_discount_grouping_cost) + (50 * a_single_normal_cost)
+            else:
+                a_total_cost = 50 * a_count  # all at normal cost as less than 3 items
+        else:
+            a_total_cost = 0
 
+        if b_count > 0:
+            if b_count//2 > 0:  # some will be at discount cost
+                b_total_discount_grouping_cost = b_count//2
+                b_single_normal_cost = b_count % 2
+                b_total_cost = (45 * b_total_discount_grouping_cost) + (30 * b_single_normal_cost)
+            else:
+                b_total_cost = 30 * b_count  # all at normal cost as less than 2 items
+        else:
+            b_total_cost = 0
 
+        if c_count > 0:
+            c_total_cost = c_count * 20
+        else:
+            c_total_cost = 0
+
+        if d_count > 0:
+            d_total_cost = d_count * 20
+        else:
+            d_total_cost = 0
+
+        total_cost = a_total_cost + b_total_cost + c_total_cost + d_total_cost    # sum costs for all items
+
+        return total_cost
     #raise NotImplementedError()
+
 
