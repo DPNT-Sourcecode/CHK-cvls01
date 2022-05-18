c9f1cc1a8fed26d6a8882706d9376b6902488179
sbx_user1051 <sbx_user1051@169.254.149.125>
2022-05-18 12:39:57 +0000

    Wed May 18 12:39:57 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,6 +4,21 @@
 # skus = unicode string
 def checkout(skus):
     skus = str(skus).upper()  # Convert string to Upper Case
+    allowed_chs = "ABCD"
+    if not all(ch in allowed_chs for ch in allowed_chs): # make sure all string values are right otherwise return -1
+
+        return -1
+    else:
+        a_count = skus.count("A")  # count occurrences of each item
+        b_count = skus.count("B")
+        c_count = skus.count("C")
+        d_count = skus.count("D")
+
+        #Total Up cost of occurrences of Item A
+
+        if a_count > 0:
+            
 
 
     #raise NotImplementedError()
+
