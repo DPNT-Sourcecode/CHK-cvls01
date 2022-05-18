b318f16af3bace3aedd4b3d33ea855b8452195d8
sbx_user1051 <sbx_user1051@169.254.149.125>
2022-05-18 10:41:28 +0000

    Wed May 18 10:41:28 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,7 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
+    x = input
+
     x = int(x)
     y = int(y)
     if 0 <= x <= 100 and 0 <= y <= 100:
@@ -8,3 +10,4 @@ def compute(x, y):
     else:
         return "Error - please check the numbers are integers between 0 and 100"
     #raise NotImplementedError()
+
--- lib/solutions/SUM/test_sum.py
+++ lib/solutions/SUM/test_sum.py
@@ -1,9 +1,9 @@
 import sum_solution
 
-
 class TestSum():
     def test_sum(self):
-        assert sum_solution.compute('a', 'b') == 4
-        print('Done')
+        assert sum_solution.compute('1', '2') == 3
+
+
 
 
