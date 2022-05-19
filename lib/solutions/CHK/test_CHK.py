3d25b8291ae72c977f54bee4623b813a345ecfd4
sbx_user1051 <sbx_user1051@169.254.28.45>
2022-05-19 08:51:15 +0000

    Thu May 19 08:51:15 UTC 2022

--- lib/solutions/CHK/test_CHK.py
+++ lib/solutions/CHK/test_CHK.py
@@ -1,5 +1,9 @@
 import checkout_solution
 
-x = checkout_solution.checkout("")
+x = checkout_solution.checkout("AAAAA")
+print("5A " + str(x))
+assert checkout_solution.checkout("AAAAA") == 200
 
-print(x)
+x = checkout_solution.checkout("BB")
+print("2B " + str(x))
+assert checkout_solution.checkout("BB") == 45
