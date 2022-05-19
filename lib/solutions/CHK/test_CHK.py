abcdc5fdd04f8a159ab4a0aae7ee63e66b841a80
sbx_user1051 <sbx_user1051@169.254.28.45>
2022-05-19 09:03:17 +0000

    Thu May 19 09:03:17 UTC 2022

--- lib/solutions/CHK/test_CHK.py
+++ lib/solutions/CHK/test_CHK.py
@@ -6,4 +6,112 @@ assert checkout_solution.checkout("AAAAA") == 200
 
 x = checkout_solution.checkout("BB")
 print("2B " + str(x))
-assert checkout_solution.checkout("BB") == 40
+assert checkout_solution.checkout("BB") == 45
+
+x = checkout_solution.checkout("C")
+print("C " + str(x))
+assert checkout_solution.checkout("C") == 20
+
+x = checkout_solution.checkout("D")
+print("D " + str(x))
+assert checkout_solution.checkout("D") == 15
+
+x = checkout_solution.checkout("EEB")
+print("EEB " + str(x))
+assert checkout_solution.checkout("EEB") == 80
+
+x = checkout_solution.checkout("FF")
+print("FF " + str(x))
+assert checkout_solution.checkout("FF") == 10
+
+x = checkout_solution.checkout("G")
+print("G" + str(x))
+assert checkout_solution.checkout("G") == 20
+
+x = checkout_solution.checkout("HHHHH")
+print("HHHHH " + str(x))
+assert checkout_solution.checkout("HHHHH") == 45
+
+x = checkout_solution.checkout("HHHHHHHHHH")
+print("HHHHHHHHHH " + str(x))
+assert checkout_solution.checkout("HHHHHHHHHH") == 80
+
+x = checkout_solution.checkout("I")
+print("I " + str(x))
+assert checkout_solution.checkout("I") == 35
+
+x = checkout_solution.checkout("J")
+print("J " + str(x))
+assert checkout_solution.checkout("J") == 45
+
+x = checkout_solution.checkout("KK")
+print("KK " + str(x))
+assert checkout_solution.checkout("KK") == 150
+
+x = checkout_solution.checkout("L")
+print("L " + str(x))
+assert checkout_solution.checkout("L") == 90
+
+x = checkout_solution.checkout("M")
+print("M " + str(x))
+assert checkout_solution.checkout("M") == 15
+
+x = checkout_solution.checkout("NNNM")
+print("NNNM " + str(x))
+assert checkout_solution.checkout("NNNM") == 120
+
+x = checkout_solution.checkout("0")
+print("0 " + str(x))
+assert checkout_solution.checkout("0") == 10
+
+x = checkout_solution.checkout("P")
+print("P " + str(x))
+assert checkout_solution.checkout("P") == 50
+
+x = checkout_solution.checkout("PPPPP")
+print("PPPPP " + str(x))
+assert checkout_solution.checkout("PPPPP") == 200
+
+x = checkout_solution.checkout("Q")
+print("Q " + str(x))
+assert checkout_solution.checkout("Q") == 30
+
+x = checkout_solution.checkout("QQQ")
+print("QQQ " + str(x))
+assert checkout_solution.checkout("QQQ") == 80
+
+x = checkout_solution.checkout("R")
+print("R " + str(x))
+assert checkout_solution.checkout("R") == 50
+
+x = checkout_solution.checkout("RRRQ")
+print("RRRQ " + str(x))
+assert checkout_solution.checkout("RRRQ") == 150
+
+x = checkout_solution.checkout("S")
+print("S " + str(x))
+assert checkout_solution.checkout("S") == 30
+
+x = checkout_solution.checkout("T")
+print("T " + str(x))
+assert checkout_solution.checkout("T") == 20
+
+x = checkout_solution.checkout("UUU")
+print("UUU " + str(x))
+assert checkout_solution.checkout("UUU") == 80
+
+x = checkout_solution.checkout("UUUU")
+print("UUUU " + str(x))
+assert checkout_solution.checkout("UUUU") == 120
+
+x = checkout_solution.checkout("VV")
+print("VV " + str(x))
+assert checkout_solution.checkout("VV") == 12
+
+x = checkout_solution.checkout("VV")
+print("VV " + str(x))
+assert checkout_solution.checkout("VV") == 120
+
+x = checkout_solution.checkout("VVV")
+print("VVV " + str(x))
+assert checkout_solution.checkout("VVV") == 120
